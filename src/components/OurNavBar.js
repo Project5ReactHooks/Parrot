54e545271200a2ee25afac4d300052c3d38ea61a
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-12-25 12:34:41 +0200

    nav responsivness

--- src/components/OurNavBar.js
+++ src/components/OurNavBar.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/OurNavBar.css";
 import { Link } from "react-router-dom";
 
 function OurNavBar() {
+  const [toggleNav, setToggleNav] = useState(false);
+  let obj = toggleNav
+    ? { display: "flex", flexDirection: "column", alignItems: "self-start" }
+    : {};
   return (
     <nav className="navMainContainer">
       <div className="logoContainer">
-        <h1>Parrot</h1>
+        <Link to="/">
+          <h1>Parrot</h1>
+        </Link>
       </div>
-      <div className="pagesLinks">
+      <ul className="burgerMenu">
+        <li>
+          <i class="fas fa-bars" onClick={() => setToggleNav(!toggleNav)}></i>
+        </li>
+      </ul>
+      {/* style={obj} */}
+      <div className="pagesLinks" style={obj}>
         <ul>
           <li>
             <Link to="/">Home</Link>
