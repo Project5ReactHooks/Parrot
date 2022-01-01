7a1619f720c02638894b21d2a7e7441e2304c767
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-01-01 18:39:21 +0200

    solve localStorage not containing userResevations

--- src/manageAccount/ManageReservations.jsx
+++ src/manageAccount/ManageReservations.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "../style/reservation.css";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ManageReservations = () => {
   const [data, setData] = useState([]);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   useEffect(() => {
     let isLoggedIn = localStorage.getItem("isLoggedIn")
       ? JSON.parse(localStorage.getItem("isLoggedIn"))
@@ -13,10 +13,12 @@ const ManageReservations = () => {
       navigate("/login");
       return;
     }
-    let data = JSON.parse(localStorage.getItem("usersReservations"));
-    let user = JSON.parse(localStorage.getItem("loggedAccount"));
-    let filteredData = data.filter((e) => e.user === user.email);
-    setData(filteredData);
+    if (localStorage.getItem("usersReservations")) {
+      let data = JSON.parse(localStorage.getItem("usersReservations"));
+      let user = JSON.parse(localStorage.getItem("loggedAccount"));
+      let filteredData = data.filter((e) => e.user === user.email);
+      setData(filteredData);
+    }
   }, [navigate]);
 
   return data?.length ? (
